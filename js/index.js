9725c2dbde469e95ad8d94d070c08890d4c6dd65
ianjsikora <ianjsikora@gmail.com>
2015-02-19 12:56:52 -0800

    Parallax Update

--- js/index.js
+++ js/index.js
@@ -7,18 +7,7 @@
         $(".page-loader div").delay(0).fadeOut();
         $(".page-loader").delay(200).fadeOut("slow");
 
-        init_scroll_navigate();
-
-        $(window).trigger("scroll");
         $(window).trigger("resize");
-
-        // Hash menu forwarding
-        if (window.location.hash){
-            var hash_offset = $(window.location.hash).offset().top;
-            $("html, body").animate({
-                scrollTop: hash_offset
-            });
-        }
 
     });
 
@@ -26,105 +15,22 @@
 
         $(window).trigger("resize");
 
-        init_parallax();
     });
 
     $(window).resize(function(){
 
-        js_height_init();
+        resize_height_init();
 
     });
-/*
-  ========================================
-  Platform ID
-  ========================================
-*/
-    var mobileTest;
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-        mobileTest = true;
-        $("html").addClass("mobile");
-    }
-    else {
-        mobileTest = false;
-        $("html").addClass("no-mobile");
-    }
-
-    var mozillaTest;
-    if (/mozilla/.test(navigator.userAgent)) {
-        mozillaTest = true;
-    }
-    else {
-        mozillaTest = false;
-    }
-    var safariTest;
-    if (/safari/.test(navigator.userAgent)) {
-        safariTest = true;
-    }
-    else {
-        safariTest = false;
-    }
-
-    // Detect touch devices
-    if (!("ontouchstart" in document.documentElement)) {
-        document.documentElement.className += " no-touch";
-    }
-
- /*
-  ========================================
-  Scroll navigation
-  ========================================
-*/
-
-    function init_scroll_navigate(){
-
-        $(".local-scroll").localScroll({
-            target: "body",
-            duration: 1500,
-            offset: 0,
-            easing: "easeInOutExpo"
-        });
-
-        var sections = $(".background-image, .split-section, .page-section");
-        var menu_links = $(".scroll-nav li a");
-
-        $(window).scroll(function(){
-
-            sections.filter(":in-viewport:first").each(function(){
-                var active_section = $(this);
-                var active_link = $('.scroll-nav li a[href="#' + active_section.attr("id") + '"]');
-                menu_links.removeClass("active");
-                active_link.addClass("active");
-            });
-
-        });
-
-    }
-
-/*
-  ========================================
-  Parallax
-  ========================================
-*/
-
-    function init_parallax(){
-
-        // Parallax
-        if (($(window).width() >= 1024) && (mobileTest == false)) {
-            $("#home").parallax("50%", 0.3);
-            $("#first-parallax").parallax("50%", 0.2);
-            $("#second-parallax").parallax("50%", 0.5);
-        }
-
-    }
 
 })(jQuery);
 
 /*
   ========================================
-  Height Adjust
+  Page Resize
   ========================================
 */
-function js_height_init(){
+function resize_height_init(){
     (function($){
         $(".resize-height-full-page").height($(window).height());
     })(jQuery);
